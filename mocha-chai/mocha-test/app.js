159e81b93ca9ee75ef4a090f8b4ba1f2eb52a1e6
Shahriar Karim Saurov <shahriarkarimsaurov@kamk.fi>
2021-04-04 16:12:15 +0300

    update

--- mocha-chai/mocha-test/app.js
+++ mocha-chai/mocha-test/app.js
@@ -1,3 +1,8 @@
-module.exports = function(){
-    return 'Hello World';
+module.exports = {
+    sayHello: function(){
+        return 'Hello world';
+    },
+    addNumbers: function(value1, value2){
+        return value1+value2;
+    }
 }
--- mocha-chai/mocha-test/test/appTest.js
+++ mocha-chai/mocha-test/test/appTest.js
@@ -1,10 +1,21 @@
 const assert = require('chai').assert;
-const app = require('../app');
-
+// const sayHello= require('../app').sayHello;
+// const addNumbers= require('../app').addNumbers;
+const App = require('../app');
 
 
 describe('App', function () {
     it('app should return', function(){
-        assert.equal(app(), 'Hello World')
+        let result =App.sayHello();
+        assert.equal(result, 'Hello world')
+    });
+    it('sayHello should return type string', function(){
+        assert.typeOf(App.sayHello(), 'string');
+    });
+    it('addNumber should return a number', function(){
+        assert.typeOf(App.addNumbers(2,3), 'number')
+    });
+    it('addNumber should be greater than 5', function(){
+        assert.isAbove(App.addNumbers(5,3), 5)
     })
 })
